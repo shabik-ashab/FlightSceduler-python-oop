d838405b091a0201ba6dbfa5e99994b4ae7ebd54
nub69 <shabik.ashab@gmail.com>
2022-11-18 22:25:21 +0600

    Airport class initialized

---
+++ airport.py
@@ -0,0 +1,11 @@
+
+class Airport:
+    def __init__(self, name, country, lat, long, rate) -> None:
+        self.name = name
+        self.country = country
+        self.lat = lat
+        self.long = long
+        self.rate = rate
+
+    def __repr__(self) -> str:
+        return f"Airport: {self.name} in: {self.country} lattitude {self.lat} longitude: {self.long} "
