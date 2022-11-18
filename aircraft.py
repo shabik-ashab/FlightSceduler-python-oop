714374745186ea38db734c37b76c31892e38e3b4
nub69 <shabik.ashab@gmail.com>
2022-11-18 22:22:08 +0600

    Aircraft class initialized

---
+++ aircraft.py
@@ -0,0 +1,14 @@
+
+class Aircraft:
+    def __init__(self, brand, code, type, flight_range) -> None:
+        self.brand = brand
+        self.code = code
+        self.type = type
+        self.flight_range = flight_range
+
+    def get_brand(self):
+        return self.brand
+
+    def __repr__(self) -> str:
+        return f"Aircraft made by: {self.brand} code: {self.code} type: {self.type} range: {self.flight_range}"
+        
