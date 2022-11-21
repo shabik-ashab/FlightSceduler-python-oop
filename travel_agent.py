d9f3b2009efe98cf5c876917fdd71f182bb7a360
nub69 <shabik.ashab@gmail.com>
2022-11-21 21:31:47 +0600

    TravelAgent class initialized

---
+++ travel_agent.py
@@ -0,0 +1,8 @@
+
+class TravelAgent:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.trips = None 
+
+    def __repr__(self) -> str:
+        return f"TravelAgent: {self.name} "
